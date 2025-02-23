4f3a514da04e49dd91cb74d9d85e6a13aab7718b
Alex <utin97@mail.ru>
2025-02-23 14:15:50 +0300

    Add hw3

---
+++ Hw3/Hw3/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw3
+{
+    public class Person
+    {
+        public Person? Mother { get; set; }
+        public Person? Father { get; set; }
+        public List<Person> Childs { get; set; }
+        public string Name { get; set; } = "no name";
+        public string Surname { get; set; } = "no surname";
+        public string Sex { get; set; } = "no sex";
+        public DateTime DateBirth { get; set; } = DateTime.Now;
+        public Person? Spouse { get; set; }
+        public Person()
+        {
+            Mother = null;
+            Father = null;
+            Childs = [];
+            Spouse = null;
+        }
+
+        public Person(string name, string surname, string sex, DateTime dateBirth, List<Person> childs, Person? Mother, Person? Father, Person? Spouse)
+        {
+            Name = name;
+            Surname = surname;
+            Sex = sex;
+            DateBirth = dateBirth;
+            Childs = childs;
+            this.Mother = Mother;
+            this.Father = Father;
+            this.Spouse = Spouse;
+        }
+        public void PrintChilds()
+        {
+            foreach (var item in Childs)
+            {
+                if (item.Sex.Equals("Woman"))
+                    Console.WriteLine($"Dought: {item}");
+                else Console.WriteLine($"Son: {item}");
+            }
+        }
+        public void PrintParents()
+        {
+            Console.WriteLine($"Father: {Father}\nMather: {Mother}");
+        }
+        public void PrintGrandfathers()
+        {
+            Console.WriteLine($"Grandfathers: {Father?.Father}\n{Mother?.Father}");
+        }
+        public void PrintGrandmothers()
+        {
+            Console.WriteLine($"Grandmothers: {Father?.Mother}\n{Mother?.Mother}");
+        }
+
+        public void PrintFammily(int indent = 5)
+        {
+
+            Console.WriteLine($"{new string('=', indent)} {"|"}{ToString()}");
+            foreach (var item in Childs)
+            {
+                item.PrintFammily(indent + 15);
+            }
+        }
+        public string GetSpouse()
+        {
+            string text;
+            if (Spouse is null)
+                text = "no spouse";
+            else text = $"{Spouse?.Name} {Spouse?.Surname}";
+            return text;
+        }
+        public override string ToString()
+        {
+            string text = GetSpouse();
+            return $"{Name} {Surname}, sex: {Sex}, date of birth: {DateBirth}, spouse: {text}";
+        }
+    }
+}
---
+++ Hw3/Hw3/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Hw3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person grandfather = new()
+            {
+                Name = "Petr",
+                Surname = "Lapeshi",
+                Sex = "Man",
+                DateBirth = DateTime.Parse("12-03-1950")
+            };
+            Person grandmother = new()
+            {
+                Name = "Anna",
+                Surname = "Aksenova",
+                Sex = "Woman",
+                DateBirth = DateTime.Parse("01-12-1955"),
+                Spouse = grandfather
+            };
+            Person mother = new(name: "Katty", surname: "Lapeshi", sex: "Woman",
+                            dateBirth: DateTime.Parse("01-02-1967"), childs: [], Mother: grandmother, Father: grandfather, Spouse: null);
+            Person brotherMother = new(name: "Victor", surname: "Lapeshi", sex: "Man",
+                            dateBirth: DateTime.Parse("01-02-1967"), childs: [], Mother: grandmother, Father: grandfather, Spouse: null);
+            Person father = new(name: "Nikola", surname: "Pitrenko", sex: "Man",
+                            dateBirth: DateTime.Parse("04-06-1970"), childs: [], Mother: null, Father: null, Spouse: mother);
+            Person sisterFather = new(name: "Jeni", surname: "Pitrenko", sex: "Woman",
+                            dateBirth: DateTime.Parse("04-06-1973"), childs: [], Mother: null, Father: null, Spouse: brotherMother);
+            Person son = new(name: "Grisha", surname: "Pitrenko", sex: "Man",
+                            dateBirth: DateTime.Parse("12-09-1990"), childs: [], Mother: mother, Father: father, Spouse: null);
+            Person dought = new(name: "Angelina", surname: "Pitrenko", sex: "Woman",
+                            dateBirth: DateTime.Parse("02-06-1993"), childs: [], Mother: mother, Father: father, Spouse: null);
+            Person son1 = new(name: "Gena", surname: "Lapeshi", sex: "Man",
+                            dateBirth: DateTime.Parse("01-02-1994"), childs: [], Mother: sisterFather, Father: brotherMother, Spouse: null);
+            Person dought1 = new(name: "Malvina", surname: "Lapeshi", sex: "Woman",
+                            dateBirth: DateTime.Parse("01-02-1999"), childs: [], Mother: sisterFather, Father: brotherMother, Spouse: null);
+            Person son2 = new(name: "Petya", surname: "Lapeshi", sex: "Man",
+                            dateBirth: DateTime.Parse("01-02-2012"), childs: [], Mother: null, Father: son, Spouse: null);
+            grandfather.Spouse = grandmother;
+            mother.Spouse = father;
+            brotherMother.Spouse = sisterFather;
+            grandfather.Childs.Add(mother);
+            grandfather.Childs.Add(brotherMother);
+            grandmother.Childs.Add(mother);
+            grandmother.Childs.Add(brotherMother);
+            father.Childs.Add(son);
+            father.Childs.Add(dought);
+            mother.Childs.Add(son);
+            mother.Childs.Add(dought);
+            brotherMother.Childs.Add(son1);
+            brotherMother.Childs.Add(dought1);
+            sisterFather.Childs.Add(son1);
+            sisterFather.Childs.Add(dought1);
+            son.Childs.Add(son2);
+            grandfather.PrintFammily();
+        }
+    }
+}

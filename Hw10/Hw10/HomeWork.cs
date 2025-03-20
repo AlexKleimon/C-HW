39f0620cdb84e0bfbb6479ab4f319ef3d694b1c2
Alex <utin97@mail.ru>
2025-03-20 18:05:27 +0300

    Add Hw10

---
+++ Hw10/Hw10/HomeWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw10
+{
+    class HomeWork
+    {
+        public static bool FindWord(string path, string word)
+        {
+            using (StreamReader sr = new(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string? tempStr = sr.ReadLine();
+                    if (tempStr != null)
+                    {
+                        if (tempStr.Contains(word))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+        }
+        public static void FindFiles(string path, string fileExtension, List<string> listPathFiles)
+        {
+            listPathFiles.AddRange(Directory.GetFiles(path, fileExtension));
+
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                FindFiles(dir, fileExtension, listPathFiles);
+            }
+        }
+    }
+}
---
+++ Hw10/Hw10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Hw10
+{
+    internal class Program
+    {
+        //Объедините две предыдущих работы (практические работы 2 и 3): поиск файла и поиск текста в файле написав утилиту которая
+        //ищет файлы определенного расширения с указанным текстом. Рекурсивно. Пример вызова утилиты: utility.exe txt текст.
+        static void Main(string[] args)
+        {
+            List<string> listPathFiles = [];
+
+            HomeWork.FindFiles("C:\\Users\\MSI\\Desktop\\C#\\", $"*.{args[0]}", listPathFiles);
+
+            foreach (string file in listPathFiles)
+            {
+                if (HomeWork.FindWord(file, args[1]) == true)
+                {
+                    Console.WriteLine(file);
+                }
+            }
+        }
+    }
+}
